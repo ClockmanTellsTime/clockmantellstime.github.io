fc375351c1ba4871bc617b039c37b97e8b23c2b2
ClockmanTellsTime <87088337+ClockmanTellsTime@users.noreply.github.com>
2022-04-23 16:57:48 -0700

    Added better readability

--- other/num2txt.js
+++ other/num2txt.js
@@ -1,79 +1,88 @@
 function num2txt(num) {
-    const suffixes = ["thousand"," million"," billion"," trillion"," quadrillion"," quintillion"," sextillion"," septillion", " octillion"," nonillion"," decillion","undecillion","duodecillion","tridecillion","quaddecillion","quindecillion","sexadecillion","septadecillion","octadecillion","nondecillion","vigintillion"];
+    //the e in the name of this function stands for easy to make for me
+    //the 2 in the name is how many hours it took me to make this function 
 
+
+    //suffixes
+    const suffixes = ["thousand"," million"," billion"," trillion"," quadrillion"," quintillion"," sextillion"," septillion", " octillion"," nonillion"," decillion"," undecillion"," duodecillion"," tridecillion"," quaddecillion"," quindecillion"," sexadecillion"," septadecillion"," octadecillion"," nondecillion"," vigintillion"];
+
+    //variables
     let n = String(num);
-    let txt = 0
+    let betterNumber = 0
 
-    let a,b,c,d = 0
-
+    //leave function early if number to small
     if (num <= 999) {return String(num)}
 
-    if (num >= 1000000000000000000000) {
-        a = parseInt(n.split("+")[1])-2
-        b = n.split("e")[0]
+    //dumb number that makes this way too hard
+    dumbNumber = 1000000000000000000000
 
-        if (b.length >= 4) {
-            c = b.slice(0,4)
-            c = c.split(".")[0] + c.split(".")[1]
+    //i dont know how this works
+    if (num >= dumbNumber) {
+        Something = parseInt(n.split("+")[1])-2
+        digetsorSMTH = n.split("e")[0]
+
+        if (digetsorSMTH.length >= 4) {
+            iDontEvenKnow = digetsorSMTH.slice(0,4)
+            iDontEvenKnow = iDontEvenKnow.split(".")[0] + iDontEvenKnow.split(".")[1]
         }
-        if (b.length == 1) {
-            c = b.slice(0)
-            a += 2
+        if (digetsorSMTH.length == 1) {
+            iDontEvenKnow = digetsorSMTH.slice(0)
+            Something += 2
         }
-        if (b.length == 3) {
-            c = b.slice(0,3)
-            c = c.split(".")[0] + c.split(".")[1]
-            a += 1
+        if (digetsorSMTH.length == 3) {
+            iDontEvenKnow = digetsorSMTH.slice(0,3)
+            iDontEvenKnow = iDontEvenKnow.split(".")[0] + iDontEvenKnow.split(".")[1]
+            Something += 1
         }
 
+        for (var i = 0; i < Something; i++) {
+            iDontEvenKnow = iDontEvenKnow + "0"
+        }
         
-        d = c
-        
+        n = iDontEvenKnow
+    }   
 
-        
-
-        var i = 0
-        while (i < a) {
-            d = d + "0"
-            i ++;
-        }
-
-        n = d
-    }
-
+    //ezpz
     if ((n.length-1) % 3 === 0){
         let first = n.charAt(0);
         let second = n.charAt(1)
         let third = n.charAt(2)
         let suffix = suffixes[((n.length-1)/3)-1]
-        txt = first +"."+second+ third+ suffix
+        betterNumber = first +"."+second+ third+ suffix
         if (third.toString() == "0"){
-            txt = first +"."+second+ suffix
+            betterNumber = first +"."+second+ suffix
         }
         if (third.toString() == "0" && second.toString() == "0"){
-            txt = first + suffix
+            betterNumber = first + suffix
         }
         
     }
 
+    //also ezpz
     if ((n.length-2) % 3 === 0) {
         let first = n.charAt(0);
         let second = n.charAt(1)
         let third = n.charAt(2)
         let suffix = suffixes[((n.length-2)/3)-1]
-        txt = first +second+"."+third+ suffix
+        betterNumber = first +second+"."+third+ suffix
         if (third.toString() == "0"){
-            txt = first +second+ suffix
+            betterNumber = first +second+ suffix
         }
     }
 
+    //ezezezpz
     if ((n.length-3) % 3 === 0) {
         let first = n.charAt(0);
         let second = n.charAt(1)
         let third = n.charAt(2)
         let suffix = suffixes[((n.length-3)/3)-1]
-        txt = first +second+ third+ suffix
+        betterNumber = first +second+ third+ suffix
     }
 
-    return txt
+    //return pogger number
+    return betterNumber
 }  
+
+
+
+
