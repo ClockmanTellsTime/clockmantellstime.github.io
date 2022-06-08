8f54573d25dcf676a635c8e455e653eaf3746197
ClockmanTellsTime <87088337+ClockmanTellsTime@users.noreply.github.com>
2022-06-07 20:41:05 -0700

    Update sus.js

--- sus.js
+++ sus.js
@@ -1,1 +1,22 @@
 //mega tool used for cracking password for school because they are only 8 digets so is ezpz 
+
+let password = 614745
+let stock = 600000
+
+
+console.time("e")
+var solve = setInterval(function() {
+    stock += 1
+
+    if (stock == password) {
+        console.log("Password found!")
+        console.log(stock, password)
+        clearInterval(solve)
+        console.timeEnd("e")
+    }
+},1)
+
+
+
+
+
